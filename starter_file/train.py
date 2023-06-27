729de265688b725cce5824e9d5b0b7fa2c936a5f
naoshi-hirata <105419001+naoshi-hirata@users.noreply.github.com>
2023-06-27 18:23:17 +0900

    Update train.py

--- starter_file/train.py
+++ starter_file/train.py
@@ -84,7 +84,7 @@
     "    # Calculate and log the AUC_weighted metric\n",
     "    predicted_probabilities = model.predict_proba(x_test)\n",
     "    auc_weighted = roc_auc_score(y_test, predicted_probabilities, average='weighted')\n",
-    "    run.log("AUC_weighted", np.float(auc_weighted))\n",
+    "    run.log('AUC_weighted', np.float(auc_weighted))\n",
 
     "    # Save the model\n",
     "    os.makedirs('outputs', exist_ok=True)\n",

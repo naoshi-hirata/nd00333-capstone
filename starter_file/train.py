138e22c09f36f9a8cacfbbe447bf65ae94fe308f
naoshi-hirata <105419001+naoshi-hirata@users.noreply.github.com>
2023-08-09 14:31:04 +0900

    Update train.py

--- starter_file/train.py
+++ starter_file/train.py
@@ -9,6 +9,7 @@ import pandas as pd
 from azureml.core import Run, Dataset
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_auc_score
+import requests
 
 # Add arguments to script
 parser = argparse.ArgumentParser()
@@ -28,13 +29,13 @@ dataset = Dataset.get_by_name(workspace, name='car evaluation data set')
 
 x = dataset.to_pandas_dataframe()
 
-buying_dict = {'vhigh': 5, 'high': 4, 'med': 3, 'low': 2}
-maint_dict = {'vhigh': 5, 'high': 4, 'med': 3, 'low': 2}
-doors_dict = {'2': 2, '3': 3, '4': 4, '5more': 5}
-persons_dict = {'2': 2, '4': 4, 'more': 5}
-lug_boot_dict = {'small': 1, 'med': 3, 'big': 5}
-safety_dict = {'low': 1, 'med': 3, 'high': 5}
-class_dict = {'unacc':2, 'acc':3, 'good':4, 'vgood':5}
+buying_dict = {'vhigh': 3, 'high': 2, 'med': 1, 'low': 0}
+maint_dict = {'vhigh': 3, 'high': 2, 'med': 1, 'low': 0}
+doors_dict = {'2': 0, '3': 1, '4': 2, '5more': 3}
+persons_dict = {'2': 0, '4': 1, 'more': 2}
+lug_boot_dict = {'small': 0, 'med': 1, 'big': 2}
+safety_dict = {'low': 0, 'med': 1, 'high': 2}
+class_dict = {'unacc':0, 'acc':1, 'good':2, 'vgood':3}
 # カテゴリカル変数を数値に変換
 x['buying'] = x['buying'].map(buying_dict)
 x['maint'] = x['maint'].map(maint_dict)
@@ -43,6 +44,8 @@ x['persons'] = x['persons'].map(persons_dict)
 x['lug_boot'] = x['lug_boot'].map(lug_boot_dict)
 x['safety'] = x['safety'].map(safety_dict)
 x['class'] = x['class'].map(class_dict)
+# Drop rows with missing values in the 'class' column
+x = x.dropna(subset=['class'])
 
 y = x['class']
 x = x.drop('class', axis=1)
@@ -58,14 +61,16 @@ model.fit(x_train, y_train)
 
 # Calculate predicted probabilities instead of predicted labels
 y_prob = model.predict_proba(x_test)
+# Convert predicted probabilities to class labels
+y_pred = np.argmax(y_prob, axis=1)
 
 # Print shapes for debugging
 print("Shape of y_test:", y_test.shape)
-print("Shape of y_prob:", y_prob.shape)
+print("Shape of y_pred:", y_pred.shape)
 print("Unique values in y_test:", np.unique(y_test))
-print("Unique values in y_prob:", np.unique(y_prob))
+print("Unique values in y_pred:", np.unique(y_pred))
 
-accuracy = accuracy_score(y_test, y_prob)
+accuracy = accuracy_score(y_test, y_pred)
 run.log("accuracy", np.float(accuracy))
 
 # Calculate ROC AUC score
